d2ce506cd66ee3815a910b00a6927197f0174d97
logic-fabric <lowic@logic-fabric.fr>
2021-01-21 23:18:13 +0100

    refactor: fresh start

--- js/main.js
+++ js/main.js
@@ -1,7 +1,2 @@
 import {} from "./components/nav.js";
-import { openhModal } from "./components/modal.js";
-
-// OPEN MODAL with sign-in button:
-const callToActionButtons = document.querySelectorAll(".c-btn--cta");
-
-callToActionButtons.forEach((btn) => btn.addEventListener("click", openhModal));
+import {} from "./components/modal.js";
