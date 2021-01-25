c4736159d4370fe2cf5ab67718e4e8996306c878
logic-fabric <lowic@logic-fabric.fr>
2021-01-25 09:50:13 +0100

    feature: close modal when clicking on (x) icon in modal

--- js/components/modal.js
+++ js/components/modal.js
@@ -1,8 +1,17 @@
 const signinButton = document.getElementById("signin-btn");
 const modal = document.getElementById("modal");
+const closeIcon = document.getElementById("close-icon");
 
+// Open modal when clicking on signin button:
 signinButton.addEventListener("click", openModal);
+
+// Close modal when clicking on (x) icon in modal:
+closeIcon.addEventListener("click", closeModal);
 
 function openModal() {
   modal.style.display = "block";
+}
+
+function closeModal() {
+  modal.style.display = "none";
 }
