d9d1afc2f9c8cb107747c6d5c4e4def8824c84bc
logic-fabric <lowic@logic-fabric.fr>
2021-01-29 11:52:10 +0100

    refactor: form.js

--- js/components/form.js
+++ js/components/form.js
@@ -1,95 +1,101 @@
-const DANGER_COLOR = "#ff4e60";
-const NEUTRAL_COLOR = "#232323";
-
-// Bypass submit:
-const modalForm = document.getElementById("modal-form");
-
-modalForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-});
-
-// Check first name validity:
 const firstName = document.getElementById("first-name");
-
-firstName.addEventListener("input", () => {
-  const errorField = document.querySelector("#first-name + p");
-
-  displayNameValidity(firstName, errorField);
-});
-
-// Check last name validity:
 const lastName = document.getElementById("last-name");
-
-lastName.addEventListener("input", () => {
-  const errorField = document.querySelector("#last-name + p");
-
-  displayNameValidity(lastName, errorField);
-});
-
-// Check email validity:
 const email = document.getElementById("email");
-
-email.addEventListener("input", () => {
-  const errorField = document.querySelector("#email + p");
-
-  if (email.validity.valid) {
-    email.style.borderColor = NEUTRAL_COLOR;
-    errorField.style.color = NEUTRAL_COLOR;
-  } else {
-    email.style.borderColor = DANGER_COLOR;
-    errorField.style.color = DANGER_COLOR;
-  }
-});
-
-// Check previous participations:
 const previousParticipations = document.getElementById(
   "previous-participations"
 );
-
-previousParticipations.addEventListener("focusout", () => {
-  const errorField = document.querySelector("#previous-participations + p");
-
-  if (
-    previousParticipations.value !== "" &&
-    Number(previousParticipations.value) >= 0 &&
-    Number(previousParticipations.value) <= 20
-  ) {
-    previousParticipations.style.borderColor = NEUTRAL_COLOR;
-    errorField.style.color = NEUTRAL_COLOR;
-  } else {
-    previousParticipations.style.borderColor = DANGER_COLOR;
-    errorField.style.color = DANGER_COLOR;
-  }
-});
-
-// Check if general conditions have been checked:
 const generalConditionsOptin = document.getElementById(
   "general-conditions-optin"
 );
 
-generalConditionsOptin.addEventListener("change", () => {
-  const errorField = document.querySelector("#general-conditions-optin ~ p");
+// Bypass submit:
+document.getElementById("modal-form").onsubmit = (e) => e.preventDefault();
 
-  if (generalConditionsOptin.checked) {
-    errorField.style.color = NEUTRAL_COLOR;
-  } else {
-    errorField.style.color = DANGER_COLOR;
-  }
+// Check first name validity:
+firstName.oninput = () => {
+  const errorField = document.querySelector("#first-name + p");
+  displayNameValidity(firstName, errorField);
+};
+
+// Check last name validity:
+lastName.oninput = () => {
+  const errorField = document.querySelector("#last-name + p");
+  displayNameValidity(lastName, errorField);
+};
+
+// Check email validity:
+email.oninput = () => {
+  const errorField = document.querySelector("#email + p");
+  displayEmailValidity(email, errorField);
+};
+
+// Check previous participations:
+previousParticipations.addEventListener("focusout", () => {
+  const errorField = document.querySelector("#previous-participations + p");
+  displayParticipationsValidity(previousParticipations, errorField);
 });
+
+previousParticipations.onchange = () => {
+  const errorField = document.querySelector("#previous-participations + p");
+  displayParticipationsValidity(previousParticipations, errorField);
+};
+
+// Check if general conditions have been checked:
+generalConditionsOptin.onchange = () => {
+  const errorField = document.querySelector("#general-conditions-optin ~ p");
+  displayOptinValidity(generalConditionsOptin, errorField);
+};
 
 /* =================
    UTILITY FUNCTIONS
    ================= */
-function isValidName(name) {
-  return name.length > 2;
-}
 
-function displayNameValidity(inputField, errorField) {
+const isValidName = (name) => {
+  return name.length > 2;
+};
+
+const isValidParticipationsQuantity = (quantity) => {
+  return (
+    previousParticipations.value !== "" &&
+    Number(previousParticipations.value) >= 0 &&
+    Number(previousParticipations.value) <= 20
+  );
+};
+
+const displayNameValidity = (inputField, errorField) => {
   if (isValidName(inputField.value)) {
-    inputField.style.borderColor = NEUTRAL_COLOR;
-    errorField.style.color = NEUTRAL_COLOR;
+    inputField.classList.remove("border-danger");
+    errorField.classList.remove("txt-danger");
   } else {
-    inputField.style.borderColor = DANGER_COLOR;
-    errorField.style.color = DANGER_COLOR;
+    inputField.classList.add("border-danger");
+    errorField.classList.add("txt-danger");
   }
-}
+};
+
+const displayEmailValidity = (emailField, errorField) => {
+  if (email.validity.valid) {
+    emailField.classList.remove("border-danger");
+    errorField.classList.remove("txt-danger");
+  } else {
+    emailField.classList.add("border-danger");
+    errorField.classList.add("txt-danger");
+  }
+};
+
+const displayParticipationsValidity = (participationsField, errorField) => {
+  if (isValidParticipationsQuantity(previousParticipations.value)) {
+    participationsField.classList.remove("border-danger");
+    errorField.classList.remove("txt-danger");
+  } else {
+    participationsField.classList.add("border-danger");
+    errorField.classList.add("txt-danger");
+  }
+};
+
+const displayOptinValidity = (optinField, errorField) => {
+  if (optinField.checked) {
+    errorField.classList.remove("txt-danger");
+  } else {
+    errorField.classList.add("txt-danger");
+  }
+};
