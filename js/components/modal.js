8034c72c8cfba85dd455bf106e707fed45f079b9
logic-fabric <lowic@logic-fabric.fr>
2021-01-29 10:50:30 +0100

    refactor: modal.js

--- js/components/modal.js
+++ js/components/modal.js
@@ -1,17 +1,15 @@
-const closeIcon = document.getElementById("close-icon");
 const modalBackground = document.getElementById("modal-bg");
 const modalWindow = document.getElementById("modal-window");
-const signinButton = document.getElementById("signin-btn");
 
 // Open modal when clicking on signin button:
-signinButton.addEventListener("click", openModal);
+document.getElementById("signin-btn").onclick = openModal;
 
 // Close modal when clicking on (x) icon in modal:
-closeIcon.addEventListener("click", closeModal);
+document.getElementById("close-icon").onclick = closeModal;
 
 // Close modal when clicking outside modal window:
-modalWindow.addEventListener("click", (e) => e.stopPropagation());
-modalBackground.addEventListener("click", closeModal);
+modalWindow.onclick = (e) => e.stopPropagation();
+modalBackground.onclick = closeModal;
 
 function openModal() {
   modalBackground.style.display = "block";
